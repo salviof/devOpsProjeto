8dd670507ebf5a4ad9b1fbaca8f7040e9c2fcc2a
Salvio Furbino Elias <salviof@gmail.com>
2017-01-10 21:56:41 -0200

    Atualização atendendendo: Tarefa não Especificada

--- publicarHomologacao.sh
+++ publicarHomologacao.sh
@@ -39,11 +39,33 @@ Para cancelar ctr+C
 ***************************PERIGO !*********************************************
 
 "
+else 
+echo "O banco de dados no servidor NÃÃOOO será destruído"
 fi 
-read 
 
 
 
+echo "-> Escreva SIM caso queira atualizar também os requisitos do projeto
+"
+read respAtualizarRequisito
+ATUALIZAR_REQUISITO=false
+if [[ $respAtualizarRequisito == *"SIM"* ]]
+then
+ATUALIZAR_REQUISITO=true
+fi
+
+if $ATUALIZAR_REQUISITO ; then
+  echo "ATENÇÃO ----> O requisito será atualizado"
+  else
+  $respAtualizarRequisito="NAO"
+  echo "O requisito NÃÃOOO será atualizado (o sim deve ser Maiusculo)" 
+fi
+
+
+
+
+
+echo "Definindo variaveis de ambiente"
 CAMINHO_WEBAPP_PROJETO=$CAMINHO_CLIENTE_SOURCE/$NOME_PROJETO/webApp
 CAMINHO_WEBAPP_REQUISITO_PROJETO=$CAMINHO_CLIENTE_SOURCE/$NOME_PROJETO/webAppRequisitos
 CAMINHO_MODEL_PROJETO=$CAMINHO_CLIENTE_SOURCE/$NOME_PROJETO/modelRegras
@@ -53,32 +75,41 @@ CAMINHO_MODEL_TARGET=$CAMINHO_MODEL_PROJETO/target
 CAMINHO_WEBAPP_REQUISITO_PROJETO_TARGET=$CAMINHO_WEBAPP_REQUISITO_PROJETO/target
 CAMINHO_SOURCE_PROJETO=$CAMINHO_CLIENTE_SOURCE/$NOME_PROJETO
 
-echo "compilando $CAMINHO_CLIENTE_SOURCE"
+echo "Iniciando compilação de: $CAMINHO_CLIENTE_SOURCE"
+
+echo "Compilando model em: $CAMINHO_MODEL_PROJETO"
 cd $CAMINHO_MODEL_PROJETO
 source /home/superBits/superBitsDevOps/devOpsProjeto/compilar.sh
+echo "Compilando webApp em: $CAMINHO_WEBAPP_PROJETO"
 cd $CAMINHO_WEBAPP_PROJETO
 source /home/superBits/superBitsDevOps/devOpsProjeto/compilar.sh
+
+if $ATUALIZAR_REQUISITO ; then
+echo "Compilando requisitos em: $CAMINHO_WEBAPP_REQUISITO_PROJETO"
 cd $CAMINHO_WEBAPP_REQUISITO_PROJETO
 source /home/superBits/superBitsDevOps/devOpsProjeto/compilar.sh
+fi
 
 
+echo "Definindo variáveis de Diretóros das pastas Targets"
 listagemWebApp=$CAMINHO_WEBAPP_TARGET/*.war
 listagemWebAppRequisito=$CAMINHO_WEBAPP_REQUISITO_PROJETO_TARGET/*.war
 
-
+echo "Verificando existencia do War para WebApp"
 if ! ls $listagemWebApp >/dev/null
         then echo "O arquivo war não foi encontrado em $listagemWebApp  "
   exit $E_BADARGS
 fi
 
-
-
+if $ATUALIZAR_REQUISITO ; then
+echo "Verificando existencia do War para requisitos"
 if ! ls $listagemWebAppRequisito >/dev/null
         then
  echo "O arquivo war não foi encontrado em $listagemWebApp  "
   exit $E_BADARGS
 fi
-
+fi
+echo "Verificando existencia do Jar de modelagem"
 listagemModel=$CAMINHO_MODEL_TARGET/*.jar
 if ! ls $listagemModel >/dev/null
         then
@@ -89,7 +120,8 @@ fi
 
 
 
-# LISTANDO ARQUIVOS WAR NA PASTA TARGET E ARMAZENANDO EM VARIAVEL
+
+echo "Listando e armazenando nomes de arquivos do projeto encontrado"
 cd $CAMINHO_WEBAPP_TARGET
 i=0
 while read line
@@ -97,9 +129,11 @@ do
     webappfile[ $i ]="$line"        
     (( i++ ))
 done < <(ls *.war )
-echo ${webappfile[0]}
-ARQUIVO_WEBAAP="${webappfile[0]}"
 
+ARQUIVO_WEBAAP="${webappfile[0]}"
+echo "Encontrado arquivo: $ARQUIVO_WEBAAP"
+
+if $ATUALIZAR_REQUISITO ; then
 cd $CAMINHO_WEBAPP_REQUISITO_PROJETO_TARGET
 i=0
 while read line
@@ -110,7 +144,8 @@ done < <(ls *.war )
 echo ${webappfile[0]}
 
 ARQUIVO_WEBAAP_REQUISITO="${webappfile[0]}"
-
+echo "Encontrado arquivo: $ARQUIVO_WEBAAP_REQUISITO"
+fi
 
 # LISTANDO ARQUIVOS JAR NA PASTA TARGET E ARMAZENANDO EM VARIAVEL
 cd $CAMINHO_MODEL_TARGET
@@ -121,8 +156,10 @@ do
     (( y++ ))
 done < <(ls *.jar )
 ARQUIVO_MODEL=${modelfile[0]}
-
+echo "Encontrado arquivo: $ARQUIVO_MODEL"
 echo "copiando arquivos de banco de dados"
+
+echo "copiando arquivo de 
 cp $CAMINHO_SOURCE_PROJETO/bancoHomologacao.sql $CAMINHO_RELEASE/$NOME_PROJETO/ -f
 
 
@@ -142,13 +179,13 @@ echo "copiando de $CAMINHO_WEBAPP_TARGET/$ARQUIVO_WEBAAP"
 echo "para $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.war"
 
 cp $CAMINHO_WEBAPP_TARGET/$ARQUIVO_WEBAAP $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.war -f
-
+cp $CAMINHO_MODEL_TARGET/$ARQUIVO_MODEL  $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.jar -f
+cp $CAMINHO_CLIENTE_RELEASE/cliente.info  $CAMINHO_RELEASE/$NOME_PROJETO
+if $ATUALIZAR_REQUISITO ; then
 echo "copiando de $CAMINHO_WEBAPP_REQUISITO_PROJETO_TARGET/$ARQUIVO_WEBAAP_REQUISITO"
 echo "para $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.req.war"
 cp $CAMINHO_WEBAPP_REQUISITO_PROJETO_TARGET/$ARQUIVO_WEBAAP_REQUISITO $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.req.war -f
-
-cp $CAMINHO_MODEL_TARGET/$ARQUIVO_MODEL  $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.jar -f
-cp $CAMINHO_CLIENTE_RELEASE/cliente.info  $CAMINHO_RELEASE/$NOME_PROJETO
+fi
 
 cd $CAMINHO_RELEASE/$NOME_PROJETO
 git pull
@@ -158,7 +195,7 @@ git push
 
 echo "Operações locais realizadas com sucesso, enviando projeto: $NOME_PROJETO"
 
-ssh git@homologacao.superkompras.com.br 'bash -s' < /home/superBits/superBitsDevOps/SCRIPTS_SERVIDOR/atualizarProjeto.sh $NOME_PROJETO
+ssh git@homologacao.superkompras.com.br 'bash -s' < /home/superBits/superBitsDevOps/SCRIPTS_SERVIDOR/atualizarProjeto.sh $NOME_PROJETO $respAtualizarRequisito
 
 if [[ $respostaUsuario == *"$frase_chave"* ]]
 then 
