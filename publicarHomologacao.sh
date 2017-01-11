4c3dda46ad9e2cff2a24d35a2ac1074d4b462f03
Salvio Furbino Elias <salviof@gmail.com>
2017-01-11 01:29:06 -0200

    Atualização atendendendo: Tarefa não Especificada

--- publicarHomologacao.sh
+++ publicarHomologacao.sh
@@ -3,17 +3,30 @@ ARGUMENTOS_ESPERADOS=2
 diretorioChamada=$1
 nomeScript=$2
 
+RESTORE='\033[0m'
+function alerta() {
+    mensagem="$1"
+    echo -e -n "\033[1;36m$mensagem"   
+    echo -en "${RESTORE}"
+    echo " "
+}
+
+
+
+
+
+
 # Verificando se o o Cliente e o Projeto foram enviados
 if [ $# -ne $ARGUMENTOS_ESPERADOS ]
 then
-  echo "Especifique o nome do cliente, e do projeto $0 ;) "
+  alerta "Especifique o nome do cliente, e do projeto $0 ;) "
   exit $E_BADARGS
 fi
 
 #Carregando variaveis de ambiente
 source /home/superBits/superBitsDevOps/VARIAVEIS/SB_VARIAVEIS_MAVEN_GIT.sh $diretorioChamada $nomeScript
 frase_chave="QUERO APAGAR TUDO"
-echo "
+alerta "
 
 ***************************ATENÇÃO*********************************************
 
@@ -28,7 +41,7 @@ read respostaUsuario
 
 if [[ $respostaUsuario == *"$frase_chave"* ]]
 then 
-echo "
+alerta "
 ***************************ATENÇÃO - ULTIMO ALERTA ********************************
 
 O BANCO DE DADOS DO SERVIDOR REMOTO SERÁ DESTRUIDO SEM BACKUP, 
@@ -40,12 +53,12 @@ Para cancelar ctr+C
 
 "
 else 
-echo "O banco de dados no servidor NÃÃOOO será destruído"
+alerta "O banco de dados no servidor NÃÃOOO será destruído"
 fi 
 
 
 
-echo "-> Escreva SIM caso queira atualizar também os requisitos do projeto
+alerta "-> Escreva SIM caso queira atualizar também os requisitos do projeto
 "
 read respAtualizarRequisito
 ATUALIZAR_REQUISITO=false
@@ -55,17 +68,17 @@ ATUALIZAR_REQUISITO=true
 fi
 
 if $ATUALIZAR_REQUISITO ; then
-  echo "ATENÇÃO ----> O requisito será atualizado"
+  alerta "ATENÇÃO ----> O requisito será atualizado"
   else
-  $respAtualizarRequisito="NAO"
-  echo "O requisito NÃÃOOO será atualizado (o sim deve ser Maiusculo)" 
+  respAtualizarRequisito="NAO"
+  alerta "O requisito NÃÃOOO será atualizado (o sim deve ser Maiusculo)" 
 fi
 
 
 
 
 
-echo "Definindo variaveis de ambiente"
+alerta "Definindo variaveis de ambiente"
 CAMINHO_WEBAPP_PROJETO=$CAMINHO_CLIENTE_SOURCE/$NOME_PROJETO/webApp
 CAMINHO_WEBAPP_REQUISITO_PROJETO=$CAMINHO_CLIENTE_SOURCE/$NOME_PROJETO/webAppRequisitos
 CAMINHO_MODEL_PROJETO=$CAMINHO_CLIENTE_SOURCE/$NOME_PROJETO/modelRegras
@@ -75,45 +88,45 @@ CAMINHO_MODEL_TARGET=$CAMINHO_MODEL_PROJETO/target
 CAMINHO_WEBAPP_REQUISITO_PROJETO_TARGET=$CAMINHO_WEBAPP_REQUISITO_PROJETO/target
 CAMINHO_SOURCE_PROJETO=$CAMINHO_CLIENTE_SOURCE/$NOME_PROJETO
 
-echo "Iniciando compilação de: $CAMINHO_CLIENTE_SOURCE"
+alerta "Iniciando compilação de: $CAMINHO_CLIENTE_SOURCE"
 
-echo "Compilando model em: $CAMINHO_MODEL_PROJETO"
+alerta "Compilando model em: $CAMINHO_MODEL_PROJETO"
 cd $CAMINHO_MODEL_PROJETO
 source /home/superBits/superBitsDevOps/devOpsProjeto/compilar.sh
-echo "Compilando webApp em: $CAMINHO_WEBAPP_PROJETO"
+alerta "Compilando webApp em: $CAMINHO_WEBAPP_PROJETO"
 cd $CAMINHO_WEBAPP_PROJETO
 source /home/superBits/superBitsDevOps/devOpsProjeto/compilar.sh
 
 if $ATUALIZAR_REQUISITO ; then
-echo "Compilando requisitos em: $CAMINHO_WEBAPP_REQUISITO_PROJETO"
+alerta "Compilando requisitos em: $CAMINHO_WEBAPP_REQUISITO_PROJETO"
 cd $CAMINHO_WEBAPP_REQUISITO_PROJETO
 source /home/superBits/superBitsDevOps/devOpsProjeto/compilar.sh
 fi
 
 
-echo "Definindo variáveis de Diretóros das pastas Targets"
+alerta "Definindo variáveis de Diretóros das pastas Targets"
 listagemWebApp=$CAMINHO_WEBAPP_TARGET/*.war
 listagemWebAppRequisito=$CAMINHO_WEBAPP_REQUISITO_PROJETO_TARGET/*.war
 
-echo "Verificando existencia do War para WebApp"
+alerta "Verificando existencia do War para WebApp"
 if ! ls $listagemWebApp >/dev/null
-        then echo "O arquivo war não foi encontrado em $listagemWebApp  "
+        then alerta "O arquivo war não foi encontrado em $listagemWebApp  "
   exit $E_BADARGS
 fi
 
 if $ATUALIZAR_REQUISITO ; then
-echo "Verificando existencia do War para requisitos"
+alerta "Verificando existencia do War para requisitos"
 if ! ls $listagemWebAppRequisito >/dev/null
         then
- echo "O arquivo war não foi encontrado em $listagemWebApp  "
+ alerta "O arquivo war não foi encontrado em $listagemWebApp  "
   exit $E_BADARGS
 fi
 fi
-echo "Verificando existencia do Jar de modelagem"
+alerta "Verificando existencia do Jar de modelagem"
 listagemModel=$CAMINHO_MODEL_TARGET/*.jar
 if ! ls $listagemModel >/dev/null
         then
-  echo "o arquivo model não foi encontrado $listagemModel  "
+  alerta "o arquivo model não foi encontrado $listagemModel  "
   exit $E_BADARGS
 fi
 
@@ -121,7 +134,7 @@ fi
 
 
 
-echo "Listando e armazenando nomes de arquivos do projeto encontrado"
+alerta "Listando e armazenando nomes de arquivos do projeto encontrado"
 cd $CAMINHO_WEBAPP_TARGET
 i=0
 while read line
@@ -131,7 +144,7 @@ do
 done < <(ls *.war )
 
 ARQUIVO_WEBAAP="${webappfile[0]}"
-echo "Encontrado arquivo: $ARQUIVO_WEBAAP"
+alerta "Encontrado arquivo: $ARQUIVO_WEBAAP"
 
 if $ATUALIZAR_REQUISITO ; then
 cd $CAMINHO_WEBAPP_REQUISITO_PROJETO_TARGET
@@ -141,10 +154,10 @@ do
     webappfile[ $i ]="$line"        
     (( i++ ))
 done < <(ls *.war )
-echo ${webappfile[0]}
+alerta ${webappfile[0]}
 
 ARQUIVO_WEBAAP_REQUISITO="${webappfile[0]}"
-echo "Encontrado arquivo: $ARQUIVO_WEBAAP_REQUISITO"
+alerta "Encontrado arquivo: $ARQUIVO_WEBAAP_REQUISITO"
 fi
 
 # LISTANDO ARQUIVOS JAR NA PASTA TARGET E ARMAZENANDO EM VARIAVEL
@@ -156,49 +169,80 @@ do
     (( y++ ))
 done < <(ls *.jar )
 ARQUIVO_MODEL=${modelfile[0]}
-echo "Encontrado arquivo: $ARQUIVO_MODEL"
-echo "copiando arquivos de banco de dados"
+alerta "Encontrado arquivo: $ARQUIVO_MODEL"
+#alerta "executando backup do servidor"
+#ssh git@marketingparaweb.com.br 'bash -s' < /home/superBits/superBitsDevOps/SCRIPTS_SERVIDOR/realizarBackup.sh $NOME_PROJETO $respAtualizarRequisito
+#alerta "Fim Bakcup no servidor"
+#alerta "Preparando repositorio para envio em $CAMINHO_RELEASE/$NOME_PROJETO"
+#cd $CAMINHO_RELEASE/$NOME_PROJETO
 
-echo "copiando arquivo de 
+#git checkout -f
+#git checkout --theirs -- .
+#git checkout -f
+
+#git pull origin master 
+
+alerta "copiando arquivos de banco de dados"
+alerta "de $CAMINHO_SOURCE_PROJETO/bancoHomologacao.sql para $CAMINHO_RELEASE/$NOME_PROJETO/ "
 cp $CAMINHO_SOURCE_PROJETO/bancoHomologacao.sql $CAMINHO_RELEASE/$NOME_PROJETO/ -f
-
-
 
 
   
 if [ ! -f "$CAMINHO_SOURCE_PROJETO/SBProjeto.prop" ]
 then
-  echo "O Arquivo SBProjeto.prop não foi encontrada na pasta raiz do projeto, execute um teste do projeto WebApp para que o arquivo seja criado automaticamente.  "
+  alerta "O Arquivo SBProjeto.prop não foi encontrada na pasta raiz do projeto, execute um teste do projeto WebApp para que o arquivo seja criado automaticamente.  "
   exit $E_BADARGS
 fi
 
 cp  $CAMINHO_SOURCE_PROJETO/SBProjeto.prop  $CAMINHO_RELEASE/$NOME_PROJETO/ -f
 
 #COPIANDO PARA PASTA DE IMPLANTAÇÃO
-echo "copiando de $CAMINHO_WEBAPP_TARGET/$ARQUIVO_WEBAAP"
-echo "para $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.war"
+alerta "copiando war WebApp "
+alerta " de: $CAMINHO_WEBAPP_TARGET/$ARQUIVO_WEBAAP"
+alerta "para $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.war"
 
 cp $CAMINHO_WEBAPP_TARGET/$ARQUIVO_WEBAAP $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.war -f
+
+alerta "copiando jar model "
+alerta " de  $CAMINHO_MODEL_TARGET/$ARQUIVO_MODEL "
+alerta "para $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.war"
+
 cp $CAMINHO_MODEL_TARGET/$ARQUIVO_MODEL  $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.jar -f
+
+
+alerta "copiando informações do cliente"
+alerta "de   $CAMINHO_CLIENTE_RELEASE/cliente.info"
+alerta "para $CAMINHO_RELEASE/$NOME_PROJETO"
+
 cp $CAMINHO_CLIENTE_RELEASE/cliente.info  $CAMINHO_RELEASE/$NOME_PROJETO
+
 if $ATUALIZAR_REQUISITO ; then
-echo "copiando de $CAMINHO_WEBAPP_REQUISITO_PROJETO_TARGET/$ARQUIVO_WEBAAP_REQUISITO"
-echo "para $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.req.war"
+alerta "copiando de "
+alerta " de: $CAMINHO_WEBAPP_REQUISITO_PROJETO_TARGET/$ARQUIVO_WEBAAP_REQUISITO"
+alerta "para $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.req.war"
 cp $CAMINHO_WEBAPP_REQUISITO_PROJETO_TARGET/$ARQUIVO_WEBAAP_REQUISITO $CAMINHO_RELEASE/$NOME_PROJETO/$NOME_PROJETO.req.war -f
 fi
 
+alerta "preparando para enviar o repositório para o servidor em $CAMINHO_RELEASE/$NOME_PROJETO"
 cd $CAMINHO_RELEASE/$NOME_PROJETO
-git pull
+
+
+
+alerta "git add --all"
 git add --all 
+alerta "git commit"
 git commit -m "Atualizavao versao $(date '+%d/%m/%Y %H:%M:%S')"
-git push
+alerta "git push origin master"
+git push origin master -f
+alerta "git push origin master"
+git push origin master 
 
-echo "Operações locais realizadas com sucesso, enviando projeto: $NOME_PROJETO"
+alerta "Operações locais realizadas com sucesso, executando atualizações no servidor $NOME_PROJETO"
 
-ssh git@homologacao.superkompras.com.br 'bash -s' < /home/superBits/superBitsDevOps/SCRIPTS_SERVIDOR/atualizarProjeto.sh $NOME_PROJETO $respAtualizarRequisito
+ssh git@marketingparaweb.com.br 'bash -s' < /home/superBits/superBitsDevOps/SCRIPTS_SERVIDOR/atualizarProjeto.sh $NOME_PROJETO $respAtualizarRequisito
 
 if [[ $respostaUsuario == *"$frase_chave"* ]]
 then 
-ssh git@homologacao.superkompras.com.br 'bash -s' < /home/superBits/superBitsDevOps/SCRIPTS_SERVIDOR/criarBancoDeDados.sh $NOME_PROJETO
+ssh git@marketingparaweb.com.br 'bash -s' < /home/superBits/superBitsDevOps/SCRIPTS_SERVIDOR/criarBancoDeDados.sh $NOME_PROJETO
 fi 
 
