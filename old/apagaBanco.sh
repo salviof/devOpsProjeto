38a255a5e85125b5f7d010ef9f473be63f7cfda4
Salvio Furbino Elias <salviof@gmail.com>
2017-02-13 23:54:17 -0200

    Atualização atendendendo: Tarefa não Especificada

--- old/apagaBanco.sh
+++
@@ -1,6 +0,0 @@
-
-
-mysqladmin processlist -u root superCompras | \
-awk '$2 ~ /^[0-9]/ {print "KILL "$2";"}' | \
-mysql -u root 
-mysqladmin -u root   drop superCompras -f
--- old2/criarComandos.sh
+++
@@ -1,2 +0,0 @@
-cp ./comandosRaizProjeto/* ../ -r -f 
-
